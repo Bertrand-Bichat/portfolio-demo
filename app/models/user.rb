80509a137c16f50f4bfa99ba4012572583fd52a7
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-06 17:25:30 +0100

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # Callbacks
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
 
   #Enums
   enumerize :role, in: [:customer, :admin], default: :customer, scope: true
