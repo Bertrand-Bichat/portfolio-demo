4e73fe64ef456552845f9ce83bf5291b83494f56
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-06 15:52:54 +0100

    WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,25 @@
-require 'faker'
+require "faker"
 
-puts 'Creating 10 fake users...'
+if Rails.env == "production"
 
-10.times do
-  user = User.new(
-    email:       Faker::Internet.email,
-    password:    Faker::Internet.password(min_length: 12)
-  )
-  user.save!
+  puts "It's forbidden !"
+
+else
+
+  puts "Delete all database instances..."
+  User.destroy_all
+
+
+  puts "Creating 10 fake users..."
+
+  10.times do
+    user = User.new(
+      email:       Faker::Internet.email,
+      password:    Faker::Internet.password(min_length: 12)
+    )
+    user.save!
+  end
+
+  puts "Finished!"
+
 end
-
-puts 'Finished!'
