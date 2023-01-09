943e5de86e209642ded00c4e40c7b29e7b3b8fa5
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-09 18:43:53 +0100

    WIP

--- spec/jobs/toggle_user_online_job_spec.rb
+++ spec/jobs/toggle_user_online_job_spec.rb
@@ -2,10 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ToggleUserOnlineJob, type: :job do
   describe "#perform_later" do
-    it "should have enqueued the job" do
+    before do
       create(:user, :with_customer_role)
+    end
+    it "should have enqueued the job on critical queue with no wait" do
       ActiveJob::Base.queue_adapter = :test
-      expect { ToggleUserOnlineJob.perform_later(User.last.id, true) }.to have_enqueued_job
+      expect { ToggleUserOnlineJob.perform_later(User.last.id, true) }.to have_enqueued_job.on_queue("critical").at(:no_wait).exactly(:once)
     end
   end
 end
