fd28d6596b0188da33753efddbc52a262634f14a
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-09 16:02:45 +0100

    WIP

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -30,5 +30,6 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:first_name) }
     it { is_expected.to validate_presence_of(:last_name) }
     it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
   end
 end
