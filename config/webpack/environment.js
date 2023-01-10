5cb493e84bd0069a6851a98fae9ddb8a1bd911f7
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-10 14:50:56 +0100

    WIP

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,5 @@
-const { environment } = require('@rails/webpacker')
-
-// Preventing Babel from transpiling NodeModules packages
-environment.loaders.delete('nodeModules');
-module.exports = environment
-
+const { resolve } = require('path');
+const { config, environment, Environment } = require('@rails/webpacker');
 const WebpackerPwa = require('webpacker-pwa');
 new WebpackerPwa(config, environment);
+module.exports = environment;
