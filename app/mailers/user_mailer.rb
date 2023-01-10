b1cd82e88f3f27f5d5d81e48285dc2d0905f0183
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-10 15:12:14 +0100

    WIP

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def notification_welcome
     @user = params[:user]
+    metadata['user-id'] = @user.id.to_s
     mail(
       to: @user.email,
       subject: "Bienvenue sur notre site !"
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
+  get '/offline', to: 'pages#offline', as: :offline
   devise_for :users, controllers: { registrations: 'registrations' }
   root to: 'pages#home'
   # devise_scope :user do
   #   root to: "devise/sessions#new"
   # end
   # get '/users', to: 'pages#home'
-  get '/offline', to: 'pages#offline', as: :offline
+
+  namespace :webhooks do
+    scope :postmark do
+      post 'delivery-email', to: 'postmark_emails#delivery'
+    end
+  end
 
   # Sidekiq Web UI, only for admins.
   authenticate :user, ->(user) { user.role.admin? } do
