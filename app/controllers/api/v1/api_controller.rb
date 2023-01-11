b6f796c096a059ab1779867c084a134d27170f88
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-11 18:03:36 +0100

    WIP

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -3,4 +3,12 @@ class Api::V1::ApiController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   respond_to :json
+
+  private
+
+  def record_not_found
+    respond_to do |format|
+      format.json { render json: { message: "Record Not Found" }, status: :not_found }
+    end
+  end
 end
