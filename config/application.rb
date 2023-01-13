ce8d85429afa8f49589dfbcc711b5ab91b09c82f
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-13 16:51:15 +0100

    WIP

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,12 @@ Bundler.require(*Rails.groups)
 module PortfolioDemo
   class Application < Rails::Application
     config.generators do |generate|
-      generate.assets false
-      generate.helper false
-      generate.test_framework :rspec, fixture: false
+      generate.assets            false
+      generate.helper            false
+      generate.test_framework    :rspec, fixture: false
+      generate.helper_specs      false
+      generate.routing_specs     false
+      generate.view_specs        false
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
