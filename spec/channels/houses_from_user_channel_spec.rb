9616f3546a589548fc1cf366c7634d16f9acf6ef
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-13 16:36:32 +0100

    WIP

--- spec/channels/houses_from_user_channel_spec.rb
+++ spec/channels/houses_from_user_channel_spec.rb
@@ -3,13 +3,14 @@ require 'rails_helper'
 RSpec.describe HousesFromUserChannel, type: :channel do
   describe 'subscription' do
     context '#with user_id' do
-      it "successfully subscribes" do
+      it 'successfully subscribes' do
         subscribe user_id: 10
         expect(subscription).to be_confirmed
       end
     end
+
     context '#without user_id' do
-      it "rejects subscription" do
+      it 'rejects subscription' do
         subscribe user_id: nil
         expect(subscription).to be_rejected
       end
