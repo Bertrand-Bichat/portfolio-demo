959959c4bf7994358a9f2156d93106aaba4016e1
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-26 15:30:19 +0100

    WIP

---
+++ app/tasks/maintenance/offline_customers_task.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Maintenance
+  class OfflineCustomersTask < MaintenanceTasks::Task
+    def collection
+      # Collection to be iterated over
+      # Must be Active Record Relation or Array
+      User.with_role(:customer)
+    end
+
+    def process(element)
+      # The work to be done in a single iteration of the task.
+      # This should be idempotent, as the same element may be processed more
+      # than once if the task is interrupted and resumed.
+      element.update_columns(online: false)
+    end
+
+    def count
+      # Optionally, define the number of rows that will be iterated over
+      # This is used to track the task's progress
+      collection.count
+    end
+  end
+end
