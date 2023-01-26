6ebf74e1e764b5415543613f1fdc3e93fc3dac0b
Bertrand Bichat <bertrand.bichat@gmail.com>
2023-01-26 17:57:28 +0100

    WIP

--- app/tasks/maintenance/offline_customers_task.rb
+++ app/tasks/maintenance/offline_customers_task.rb
@@ -5,8 +5,7 @@ module Maintenance
     def collection
       # Collection to be iterated over
       # Must be Active Record Relation or Array
-  # discard_on ActiveJob::DeserializationError
-      User.where(role: "customer")
+      User.where(role: "customer", online: true)
     end
 
     def process(element)
